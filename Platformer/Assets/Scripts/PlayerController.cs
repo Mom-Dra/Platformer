82a03e5def49060b81775f6320d2e14b9d216835
Mom-Dra <yem9087@gmail.com>
2024-08-07 15:46:45 +0900

    플레이어 입력과 움직임 완료

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float jumpForce;
+
     private Rigidbody rb;
     private Vector3 addPos;
+
+    private bool canJump;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        canJump = true;
     }
 
     private void FixedUpdate()
     {
-        //rb.Move()
+        rb.MovePosition(rb.position + addPos * Time.fixedDeltaTime * speed);
 
-        rb.MovePosition(rb.position + addPos);
+        Debug.DrawRay(rb.position, Vector3.down * (transform.lossyScale.y + 0.01f), Color.red);
+        Debug.DrawRay(rb.position, transform.forward, Color.red);
+
+        if (Physics.Raycast(rb.position, Vector3.down, transform.lossyScale.y + 0.01f, LayerMask.GetMask("Floor")))
+        {
+            Debug.Log("true·Î!");
+            canJump = true;
+        }
+        else canJump = false;
     }
 
     private void OnArrow(InputValue value)
@@ -27,11 +44,16 @@ public class PlayerController : MonoBehaviour
 
         Debug.Log(val);
 
-        addPos = new Vector3(val.x, 0f, val.y);
+        addPos = new Vector3(val.x, 0f, 0f).normalized;
     }
 
     private void OnJump(InputValue value)
     {
-        Debug.Log("OnJump");
+        if (!canJump) return;
+
+        Debug.Log($"false·Î");
+
+        canJump = false;
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
