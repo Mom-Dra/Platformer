78a76fbc2d0578cbccb2b97ce2a817f4efa2c350
Mom-Dra <yem9087@gmail.com>
2024-08-09 21:16:00 +0900

    플레이어 AI 구현2

--- Platformer/Assets/Scripts/EnemyController.cs
+++ Platformer/Assets/Scripts/EnemyController.cs
@@ -2,18 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ABC
-{
-    private float a;
-    private float b;
-    private float c;
+enum EnemyState
+{ 
+    Move,
+    Detect,
+    Attack
 }
+
 
 public class EnemyController : MonoBehaviour
 {
+    private Rigidbody rigid;
+
     [SerializeField]
     private float speed;
 
     [SerializeField]
     private float detectDistance;
+
+    [SerializeField]
+    private Floor floor;
+
+    private Vector3 dir;
+
+    private EnemyState state;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        dir = Vector3.right;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        switch (state)
+        {
+            case EnemyState.Move:
+                Move();
+                break;
+            case EnemyState.Detect:
+                break;
+            case EnemyState.Attack:
+                break;
+        }
+    }
+
+    private void OnEnable()
+    {
+        floor.hasPlayerChanged += FloorHasPlayer;
+    }
+
+    private void OnDisable()
+    {
+        floor.hasPlayerChanged -= FloorHasPlayer;
+    }
+
+    private void Move()
+    {
+        Vector3 nextPos = rigid.position + dir * Time.fixedDeltaTime * speed;
+        nextPos.x = Mathf.Clamp(nextPos.x, floor.GetLeft(), floor.GetRight());  
+        
+        if(nextPos.x >= floor.GetRight())
+        {
+            dir = Vector3.left;
+        }
+        else if (nextPos.x <= floor.GetLeft())
+        {
+            dir = Vector3.right;
+        }
+
+        rigid.MovePosition(nextPos);
+    }
+
+    private void Detect()
+    {
+        Debug.Log("Detect");
+    }
+
+    private void FloorHasPlayer(bool b)
+    {
+        
+    }
 }
--- Platformer/Assets/Scripts/Floor.cs
+++ Platformer/Assets/Scripts/Floor.cs
@@ -1,26 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Floor : MonoBehaviour
-{
-    static List<Floor> floorList = new List<Floor>();
-    LinkedList<Floor> l;
-        
-    private GameObject game;
+{        
+    private static LinkedList<Floor> floors = new LinkedList<Floor>();
 
+    [SerializeField]
     private bool hasPlayer;
 
     public UnityAction<bool> hasPlayerChanged;
 
     private void Awake()
     {
-        floorList.Add(this);
+        floors.AddLast(this);
+    }
+
+    private void SetHasPlayer(bool b)
+    {
+        if (hasPlayer != b) NotifyHasPlayerChanged(b);
+
+        hasPlayer = b;
     }
 
     private void NotifyHasPlayerChanged(bool changed)
     {
-        hasPlayerChanged.Invoke(changed);
+        hasPlayerChanged?.Invoke(changed);
+    }
+
+    public float GetLeft()
+    {
+        return transform.position.x - transform.lossyScale.x * 0.5f;
+    }
+
+    public float GetRight()
+    {
+        return transform.position.x + transform.lossyScale.x * 0.5f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            foreach (Floor floor in floors)
+            {
+                if(floor.hasPlayer && floor != this)
+                {
+                    floor.SetHasPlayer(false);
+                }
+            }
+
+            SetHasPlayer(true);
+        }
     }
 }
